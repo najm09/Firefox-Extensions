b392be46f6cb7d0b1c01deaaf7c78d81ddf53583
najm09 <fnajm09@gmail.com>
2021-11-09 16:09:59 +0530

    first commit

---
+++ changeColorExt/borderify.js
@@ -0,0 +1,6 @@
+document.body.style.backgroundColor = "tomato";
+window.alert("Hey there");
+window.alert("Big surprise for you");
+for(var i = 0; i >= 0; ++i ){
+  window.alert("I'll not let you work untill you fix this issue");
+}
